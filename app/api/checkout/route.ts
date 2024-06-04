c4044093c5ab300af18e1c29cb007d03db37f895
Mateo <mateobonavia@gmail.com>
2024-06-04 17:38:13 -0300

    deleted implementation of mercado pago

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -1,9 +1,4 @@
 import { NextRequest, NextResponse } from "next/server";
-import { MercadoPagoConfig, Preference } from "mercadopago";
-
-const client = new MercadoPagoConfig({
-  accessToken: `${process.env.MP_ACCESS_TOKEN}`,
-});
 
 const corsHeaders = {
   "Access-Control-Allow-Origin": "*",
@@ -17,54 +12,13 @@ export async function OPTIONS() {
 
 export async function POST(req: NextRequest, res: NextResponse) {
   try {
-    const { cartItems, customer } = await req.json();
-
-    if (!cartItems || !customer) {
-      return new NextResponse("Not enough data to checkout", {
-        status: 400,
-        headers: corsHeaders,
-      });
-    }
-
-    const total = cartItems.reduce(
-      (acc: number, cartItem: any) =>
-        acc + cartItem.item.price * cartItem.quantity,
-      0
-    );
-
     //
     // Función para el checkout en MercadoPago. Guardar clerkId para poder obtener info del cliente ⬆
-
-    const body = {
-      items: [
-        {
-          id:
-            cartItems.length > 1
-              ? Number(cartItems[0].item._id) * Math.random()
-              : cartItems[0].item._id,
-          title:
-            cartItems.length > 1
-              ? cartItems.map((item: ProductType) => item.title).join(", ")
-              : cartItems[0].title,
-          quantity: 1,
-          unit_price: Number(total),
-          currency_id: "ARS",
-        },
-      ],
-      back_urls: {
-        success: "https://www.google.com/?hl=es",
-        pending: "https://www.google.com/?hl=es",
-        failure: "https://www.google.com/?hl=es",
-      },
-      auto_return: "approved",
-    };
-    const preference = new Preference(client);
-    const result = await preference.create({ body });
 
     //
     // En el return mandamos lo necesario para MercadoPago y en el header el cors para evitar problemas. ⬇
     //
-    return NextResponse.json(result, { status: 200, headers: corsHeaders });
+    return NextResponse.json({ status: 200, headers: corsHeaders });
     // return new NextResponse(result.id, { status: 200, headers: corsHeaders });
   } catch (error) {
     console.log("[checkout_POST", error);
