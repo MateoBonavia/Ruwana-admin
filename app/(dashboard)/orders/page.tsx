6a91345ea6b1ad129fa75ece12dcd45a31743c02
Mateo <mateobonavia@gmail.com>
2024-06-11 10:50:23 -0300

    resolved problem -non updated when new order was created-

--- app/(dashboard)/orders/page.tsx
+++ app/(dashboard)/orders/page.tsx
@@ -1,12 +1,34 @@
+"use client";
 import { DataTable } from "@/components/custom ui/DataTable";
+import Loader from "@/components/custom ui/Loader";
 import { columns } from "@/components/orders/OrderColumns";
 import { Separator } from "@/components/ui/separator";
 
-const Orders = async () => {
-  const res = await fetch("http://localhost:3000/api/orders");
-  const orders = await res.json();
+import { useEffect, useState } from "react";
 
-  return (
+const Orders = () => {
+  const [loading, setLoading] = useState(true);
+  const [orders, setOrders] = useState([]);
+
+  const getOrders = async () => {
+    try {
+      const res = await fetch("/api/orders");
+      const orders = await res.json();
+
+      setOrders(orders);
+      setLoading(false);
+    } catch (error) {
+      console.log("[orders_GET]", error);
+    }
+  };
+
+  useEffect(() => {
+    getOrders();
+  }, []);
+
+  return loading ? (
+    <Loader />
+  ) : (
     <div className="px-10 py-5">
       <p className="text-heading2-bold">Ordenes</p>
       <Separator className="bg-grey-1 my-5" />
