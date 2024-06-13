73e978f3929d6146a6c55a82d6d82dc54c593963
Mateo <mateobonavia@gmail.com>
2024-06-13 13:14:54 -0300

    ready download excle with info | missing review for more details

--- utils/exportToExcel.ts
+++ utils/exportToExcel.ts
@@ -1,39 +1,60 @@
 import ExcelJs from "exceljs";
 
-export const exportOrdersToExcel = async (orders: any[]) => {
+export const exportOrdersToExcel = async (orders: OrderColumnType[]) => {
   const workbook = new ExcelJs.Workbook();
   const worksheet = workbook.addWorksheet("Ruwana_Carteras_Ordenes");
-  console.log(orders);
 
   worksheet.columns = [
-    { header: "ID", key: "_id", width: 20 },
+    { header: "ID", key: "_id", width: 30 },
     { header: "Cliente", key: "customer", width: 20 },
-    { header: "Cantidad", key: "quantity", width: 20 },
-    { header: "Importe", key: "totalAmount", width: 20 },
-    // { header: "Producto", key: "product", width: 20 },
-    { header: "Fecha", key: "createdAt", width: 20 },
+    { header: "Email", key: "email", width: 30 },
+    { header: "Cantidad de productos", key: "productsQuantity", width: 25 },
+    { header: "Producto/s", key: "products", width: 30 },
+    { header: "Importe", key: "totalAmount", width: 15 },
+    { header: "Fecha", key: "createdAt", width: 30 },
   ];
 
-  orders.forEach((order) => {
-    worksheet.addRow({
-      _id: order._id,
-      customer: order.customer,
-      quantity: order.products,
-      totalAmount: order.totalAmount,
-      // product: order.product,
-      createdAt: order.createdAt,
+  Promise.all(
+    orders.map(async (order) => {
+      const res = await fetch(`/api/orders/${order._id}`);
+      const { orderDetails, customer } = await res.json();
+      return {
+        id: orderDetails._id,
+        customer: customer.name,
+        email: customer.email,
+        productsQuantity: orderDetails.products.length,
+        products: orderDetails.products
+          .map((productDetail: any) => productDetail.product.title)
+          .join(", "),
+        totalAmount: orderDetails.totalAmount,
+        createdAt: orderDetails.createdAt,
+      };
+    })
+  )
+    .then((completedOrders) => {
+      completedOrders.forEach((orderForExcel) => {
+        worksheet.addRow({
+          _id: orderForExcel.id,
+          customer: orderForExcel.customer,
+          email: orderForExcel.email,
+          productsQuantity: orderForExcel.productsQuantity,
+          products: orderForExcel.products,
+          totalAmount: orderForExcel.totalAmount,
+          createdAt: orderForExcel.createdAt,
+        });
+      });
+    })
+    .then(async () => {
+      const buffer = await workbook.xlsx.writeBuffer();
+
+      const blob = new Blob([buffer], {
+        type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "Ruwana_Ordenes.xlsx";
+      a.click();
+      URL.revokeObjectURL(url);
     });
-  });
-
-  //   const buffer = await workbook.xlsx.writeBuffer();
-
-  //   const blob = new Blob([buffer], {
-  //     type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-  //   });
-  //   const url = URL.createObjectURL(blob);
-  //   const a = document.createElement("a");
-  //   a.href = url;
-  //   a.download = "Ruwana_Ordenes.xlsx";
-  //   a.click();
-  //   URL.revokeObjectURL(url);
 };
